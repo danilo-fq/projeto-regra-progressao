6dc19735ae37041a69b201ea4e8007a52cf9d2d2
Danilo Ferreira Queiroz <danilof.queiroz1@gmail.com>
2024-04-16 15:58:07 -0300

    feat: create the adding activities logic

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,19 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+
+    int examsNumber = Integer.parseInt(scanner.nextLine());
+
+    for(int index = 1; index <= examsNumber; index += 1) {
+      System.out.println("Digite o nome da atividade " + index + ":");
+      String examName = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + index + ":");
+      Integer examWeight = Integer.parseInt(scanner.nextLine());
+    }
+
+    scanner.close();
   }
 }
